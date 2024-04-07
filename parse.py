25e6915011fd906ae8c3f3057fa764e751ed7557
Yuzuki Arai <arai@yudukikun5120.me>
2024-04-07 20:36:03 +0900

    refactor: add type annotation and dataclass, logging

--- parse.py
+++ parse.py
@@ -1,40 +1,81 @@
+from dataclasses import dataclass, asdict, field
+import csv
 import json
+from typing import ClassVar, Dict, List
+import logging
 
-# Parse csv to kdb.json
-with open("kdb.csv", "r", encoding="utf_8") as f:
-    l = []
-    lines = f.readlines()
-    # remove the header
-    lines.pop(0)
-    for line in lines:
-        tmp = line.split('"')
-        class_id = tmp[1]
-        name = tmp[3]
-        module = tmp[11]
-        period = tmp[13]
-        room = tmp[15]
-        remarks = tmp[21]
+# ログの設定
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-        if room == "":
-            room = " "
 
-        unit = tmp[7]
-        unit = unit.replace(" ", "")
-        class_data = [class_id, name, module, period, room, remarks, unit]
+@dataclass
+class Subject:
+    class_id: str
+    name: str
+    module: str
+    period: str
+    remarks: str
+    unit: str
+    room: str = field(default=" ")  # デフォルト値を指定
 
-        if not "" in set(class_data):
-            l.append(class_data)
+    CSV_HEADER: ClassVar[List[str]] = [
+        "科目番号",
+        "科目名",
+        "授業方法",
+        "単位数",
+        "標準履修年次",
+        "実施学期",
+        "曜時限",
+        "教室",
+        "担当教員",
+        "授業概要",
+        "備考",
+        "科目等履修生申請可否",
+        "申請条件",
+        "短期留学生申請可否",
+        "申請条件",
+        "英語(日本語)科目名",
+        "科目コード",
+        "要件科目名",
+        "データ更新日",
+    ]
 
-json_data = {}
-l.pop(0)
+    @classmethod
+    def from_csv_row(cls, row: Dict[str, str]) -> "Subject":
+        return cls(
+            class_id=row["科目番号"],
+            name=row["科目名"],
+            module=row["実施学期"],
+            period=row["曜時限"],
+            room=row.get("教室", " "),
+            remarks=row["備考"],
+            unit=row["単位数"].replace(" ", ""),
+        )
 
-for i in l:
-    json_data[i[0]] = i[1:]
 
-# { "科目番号": ["科目名","モジュール","曜時限","教室","備考","単位数"], ・・・}
-enc = json.dumps(json_data, ensure_ascii=False, indent=2)
+def convert_csv_to_json(csv_file_path: str, json_file_path: str) -> None:
+    data = {}
+    with open(csv_file_path, mode="r", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            subject = Subject.from_csv_row(row)
+            # 科目番号をキーとして、科目情報をリストとして格納
+            # { "科目番号": ["科目名","モジュール","曜時限","教室","備考","単位数"], ・・・}
+            data[subject.class_id] = [
+                subject.name,
+                subject.module,
+                subject.period,
+                subject.room,
+                subject.remarks,
+                subject.unit,
+            ]
 
-with open("kdb.json", "w") as f:
-    f.write(enc)
+    with open(json_file_path, mode="w", encoding="utf-8") as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=2)
+    logging.info(f"CSVデータを'{json_file_path}'へ変換しました。")
 
-print("complete")
+
+if __name__ == "__main__":
+    convert_csv_to_json("kdb.csv", "kdb.json")
