c81a1e431fface30098b7421d9cef86655d41b46
Yuzuki Arai <arai@yudukikun5120.me>
2024-04-09 03:35:47 +0900

    test: add cases for kdb_twinc

--- test_parse.py
+++ test_parse.py
@@ -12,6 +12,8 @@ import yaml
 from parse import convert_csv_to_json
 from parse_structural import convert_csv_to_structural_json
 from parse2yaml import convert_csv_to_yaml
+from parse_twinc import convert_csv_to_twinc_json
+
 
 class TestCSVtoJSONConversion(unittest.TestCase):
     """
@@ -156,5 +158,102 @@ class TestCSVtoJSONConversion(unittest.TestCase):
         # testdata/test_kdb_structural_output.jsonを削除
         os.remove(output_path)
 
-if __name__ == '__main__':
+    def test_kdb_twinc_ja_json(self):
+        """
+        Test the conversion of CSV to JSON for the Japanese language.
+
+        This method performs the following steps:
+        1. Defines the paths for input CSV file, output JSON file, and expected output JSON file.
+        2. Executes the conversion by calling the `convert_csv_to_json` function.
+        3. Verifies the conversion result by comparing the actual output JSON with the expected output JSON.
+        4. Performs additional detailed validation on the converted data.
+
+        Raises:
+            AssertionError: If the conversion result does not match the expected output.
+
+        """
+        input_csv_path = "testdata/sample_kdb.csv"
+        output_json_path = "testdata/test_kdb_twinc.json"
+        expected_output_path = "testdata/sample_kdb_twinc.json"
+
+        # 変換を実行
+        convert_csv_to_twinc_json(
+            name_field_number=3, input_file=input_csv_path, output_file=output_json_path
+        )
+
+        # 変換結果の検証
+        with open(output_json_path, mode="r", encoding="utf-8") as f:
+            actual_data = json.load(f)
+
+        # 期待される結果との比較
+        with open(expected_output_path, mode="r", encoding="utf-8") as f:
+            expected_data = json.load(f)
+
+        # データが辞書であることを確認
+        self.assertIsInstance(actual_data, dict)
+        self.assertIsInstance(expected_data, dict)
+
+        # 出力が期待される結果と一致することを確認
+        self.assertEqual(actual_data, expected_data)
+
+        # さらに詳細な検証を行う
+        for class_id, info in actual_data.items():
+            self.assertIsInstance(class_id, str)
+            self.assertIsInstance(info, dict)
+            self.assertEqual(len(info), 6)
+
+        # testdata/test_kdb_twinc_ja.jsonを削除
+        os.remove(output_json_path)
+
+    def test_kdb_twinc_en_json(self):
+        """
+        Test the conversion of CSV to JSON for the English language.
+
+        This method performs the following steps:
+        1. Defines the paths for input CSV file, output JSON file, and expected output JSON file.
+        2. Executes the conversion by calling the `convert_csv_to_json` function.
+        3. Verifies the conversion result by comparing the actual output JSON with the expected output JSON.
+        4. Performs additional detailed validation on the converted data.
+
+        Raises:
+            AssertionError: If the conversion result does not match the expected output.
+
+        """
+        input_csv_path = "testdata/sample_kdb.csv"
+        output_json_path = "testdata/test_kdb_twinc_en.json"
+        expected_output_path = "testdata/sample_kdb_twinc_en.json"
+
+        # 変換を実行
+        convert_csv_to_twinc_json(
+            name_field_number=31,
+            input_file=input_csv_path,
+            output_file=output_json_path,
+        )
+
+        # 変換結果の検証
+        with open(output_json_path, mode="r", encoding="utf-8") as f:
+            actual_data = json.load(f)
+
+        # 期待される結果との比較
+        with open(expected_output_path, mode="r", encoding="utf-8") as f:
+            expected_data = json.load(f)
+
+        # データが辞書であることを確認
+        self.assertIsInstance(actual_data, dict)
+        self.assertIsInstance(expected_data, dict)
+
+        # 出力が期待される結果と一致することを確認
+        self.assertEqual(actual_data, expected_data)
+
+        # さらに詳細な検証を行う
+        for class_id, info in actual_data.items():
+            self.assertIsInstance(class_id, str)
+            self.assertIsInstance(info, dict)
+            self.assertEqual(len(info), 6)
+
+        # testdata/test_kdb_twinc_en.jsonを削除
+        os.remove(output_json_path)
+
+
+if __name__ == "__main__":
     unittest.main()
